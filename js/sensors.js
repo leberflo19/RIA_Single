3a27e964c950f79aaff753cb09cbfe05136e3268
leberflo19 <75506136+leberflo19@users.noreply.github.com>
2020-12-06 10:45:06 +0100

    Added Vibration

--- js/sensors.js
+++ js/sensors.js
@@ -1,15 +1,25 @@
 jQuery(document).ready(function() {
         if (window.DeviceOrientationEvent) {
-        window.addEventListener("deviceorientation", function(event) {
-            // alpha: rotation around z-axis
-            var rotateDegrees = event.alpha;
-            // gamma: left to right
-            var leftToRight = event.gamma;
-            // beta: front back motion
-            var frontToBack = event.beta;
-            $("#alpha").text(rotateDegrees);
-            $("#beta").text(leftToRight);
-            $("#gamma").text(frontToBack);
-        }, true);
+            window.addEventListener("deviceorientation", function(event) {
+                // alpha: rotation around z-axis
+                var rotateDegrees = event.alpha;
+                // gamma: left to right
+                var leftToRight = event.gamma;
+                // beta: front back motion
+                var frontToBack = event.beta;
+                $("#alpha").text(rotateDegrees);
+                $("#beta").text(leftToRight);
+                $("#gamma").text(frontToBack);
+            }, true);
+
+           
     }
 });
+
+function vibrate() {
+    navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+
+    if (navigator.vibrate) {
+        navigator.vibrate([500, 300, 100]);
+    }
+}
